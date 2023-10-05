0969924f45bc09c2666dd03c19d6ee530e069643
Khakim Hudaya <hamas182@gmail.com>
2023-10-05 10:35:52 +0800

    Create setup-docker.sh

---
+++ setup-docker.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce
+docker --version
+
+
+# install docker-compose 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
